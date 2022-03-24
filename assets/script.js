a17b76c0b2e13c32dc619eae2f473017ad2bfd26
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-03-24 00:00:03 -0400

    started working on script file, adding the frist set of variables.

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// creating all variables for the game //
+var intro = document.querySelector(".intro");
+var win = document.querySelector(".win");
+var lose = document.querySelector(".lose");
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
