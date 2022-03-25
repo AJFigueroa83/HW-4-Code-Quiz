544e0f404d38e8fd6599bfdc56f856a5931a275e
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-03-24 23:59:29 -0400

    added init function, resetGame function and event listener to start game and reset game.

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,32 @@ var win = document.querySelector(".win");
 var lose = document.querySelector(".lose");
 var timerElement = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
+
+
+var winCounter = 0;
+var loseCounter = 0;
+var timer;
+var timerCount;
+
+// this function is called when the page loads 
+function init() {
+    getWins();
+    getlosses();
+  }
+
+// Attached event listener to start button to call startGame function on click
+startButton.addEventListener("click", startGame);
+
+// Added reset button
+var restartGame = document.querySelector(".restart-game");
+
+function resetGame() {
+  // Resets win and loss counts
+  winCounter = 0;
+  loseCounter = 0;
+  // Renders win and loss counts and sets them into client storage
+  setWins()
+  setLosses()
+}
+// Attached event listener to button
+resetButton.addEventListener("click", resetGame);
