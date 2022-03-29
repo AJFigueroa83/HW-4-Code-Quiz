d849bf9317e7c2aa3c4ee62f0e00028bef053582
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-03-29 18:32:04 -0400

    ran through some bug issues and resolved major ones that were not allowing me to view the questions. still have a few more to take care of.

--- assets/script.js
+++ assets/script.js
@@ -8,71 +8,103 @@ var questionEl = document.querySelector("#question");
 var answerButtonsEl = document.querySelector("#answer-buttons");
 var checkAnswerEl = document.querySelector("#check-answer");
 var viewHighScores = document.querySelector("highscores-link");
-var submitButton = document.querySelector("submit-btn");
-var clearScoreButton = document.querySelector("clear-btn");
-var initialsField = document.querySelector("player-name");
-var restartButton = document.querySelector("restart-btn");
-var scoreField = document.querySelector("player-score");
-var scores = JSON.parse(localStorage.getItem("scores")) || [];
+var submitButton = document.querySelector("#submit-btn");
+var clearScoreButton = document.querySelector("#clear-btn");
+var initialsField = document.querySelector("#player-name");
+var restartButton = document.querySelector("#restart-btn");
+var scoreField = document.querySelector("#player-score");
+var scores = JSON.parse(localStorage.getItem("#scores")) || [];
 
-var shuffledQuestions, currentQuestionIndex;
-var timeLeft = 60
+var shuffledQuestions = []; 
+var currentQuestionIndex = 0;
+var timeLeft = 60;
 var timerID;
 
 // Created the questions to be populated on the quiz //
 var questions = [
     {
     question: 'Which of these is not a red grape?',
-    answers: ['Pinot Noir', 'Pinot Grigio', 'Pinot Meunier', 'Pinot Nero'],
-    answer: [1],
+    answers: [
+        { text: 'Pinot Noir', correct: false },
+        { text: 'Pinot Grigio', correct: true },
+        { text: 'Pinot Meunier', correct: false },
+        { text: 'Pinot Nero', correct: false }]
     },
     {    
     question: 'If a champagne is labeled "Blanc de Blacns", which grape is it made from?',
-    answers: ['Pinot Grigio', 'Chardonnay', 'Sauvignon Blanc', 'Pinot Gris'],
-    answer: [1],
+    answers: [
+        { text: 'Pinot Grigio', correct: false },
+        { text: 'Chardonnay', correct: true },
+        { text: 'Sauvignon Blanc', correct: false },
+        { text: 'Pinot Gris', correct: false }]
     },
     {    
     question: 'Which grape is Australia most famously known for?',
-    answers: ['Shiraz', 'Cabernet Sauvignon', 'Pinot Noir', 'Chardonnay'],
-    answer: [0],
+    answers: [
+        { text: 'Shiraz', correct: true },
+        { text: 'Cabernet Sauvignon', correct: false },
+        { text: 'Pinot Noir', correct: false }, 
+        { text: 'Chardonnay', correct: false }]
     },
     {    
     question: 'What does "LBV" stand for?',
-    answers: ['Last Bottled Variant', 'Latest Bottled Vine', 'Late Bottled Vintage', 'Last Built Vinyard'],
-    answer: [2],
+    answers: [
+        { text: 'Last Bottled Variant', correct: false },
+        { text: 'Latest Bottled Vine', correct: false },
+        { text: 'Late Bottled Vintage', correct: true },
+        { text: 'Last Built Vinyard', correct: false }]
     },
     {    
     question: 'What is the main ingredient in producing Cognac?',
-    answers: ['grapes', 'grains', 'barley', 'hops'],
-    answer: [0],
+    answers: [
+        { text: 'grapes', correct: true },
+        { text: 'grains', correct: false },
+        { text: 'barley', correct: false },
+        { text: 'hops', correct: false }]
     },
     {    
     question: 'Where does Cognac come from?',
-    answers: ['Spain', 'United States', 'Italy', 'France'],
-    answer: [3],
+    answers: [
+        { text: 'Spain', correct: false },
+        { text: 'United States', correct: false },
+        { text: 'Italy', correct: false },
+        { text: 'France', correct: true }]
     },
     {    
     question: 'Italy produces a spirit made from grape must. What is it called?',
-    answers: ['Grappa', 'Marx', 'Orujo', 'Porto'],
-    answer: [0],
+    answers: [
+        { text: 'Grappa', correct: true },
+        { text: 'Marx', correct: false },
+        { text: 'Orujo', correct: false },
+        { text: 'Porto', correct: false }]
     },
     {
     question: 'What is the main ingredient of Tequila?',
-    answers: ['barley', 'agave', 'aloe', 'grains'],
-    answer: [1],
+    answers: [
+        { text: 'barley', correct: false },
+        { text: 'agave', correct: true },
+        { text: 'aloe', correct: false },
+        { text: 'grains', correct: false }]
     },
     {
     question: 'Which country is most famously known for their whisky?',
-    answers: ['Ireland', 'Canada', 'Scotland', 'Japan'],
-    answer: [2],
+    answers: [
+        { text: 'Ireland', correct: false },
+        { text: 'Canada', correct: false },
+        { text: 'Scotland', correct: true },
+        { text: 'Japan', correct: false }]
     },
     {
     question: 'What is the most widely consumed spirit?',
-    answers: ['vodka', 'tequila', 'rum', 'whiskey'],
-    answer: [0],
+    answers: [
+        { text: 'vodka', correct: true },
+        { text: 'tequila', correct: false },
+        { text: 'rum', correct: false },
+        { text: 'whiskey', correct: false }]
     },
 ]
 
+// added event listeners for start and reset game //
 startButton.addEventListener("click", startGame);
 nextButton.addEventListener("click", () => {
     currentQuestionIndex++
@@ -102,12 +134,16 @@ setNextQuestion();
 
 function setNextQuestion() {
     resetState();
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
+    showQuestion(questions[currentQuestionIndex]);
+}
+
+function shuffleQuestions() {
+    Math.floor(Math.random() * questions.length)
 }
 
 function showQuestion(question) {
     questionEl.innerText = question.question
-    question.answers.array.forEach(answer => {
+    question.answers.forEach(answer => {
         var button = document.createElement("button")
         button.innerText = answer.text
         button.classList.add("btn")
@@ -127,7 +163,7 @@ function resetState() {
     }
 }
 function selectAnswer(e) {
-    var selectedButton = e.traget;
+    var selectedButton = e.target;
     var correct = selectedButton.dataset.correct;
     checkAnswerEl.classList.remove("hide")
     if (correct) {
@@ -141,12 +177,15 @@ function selectAnswer(e) {
             timeLeft -= 10;
         }
     }
+    currentQuestionIndex ++
+    setNextQuestion()
 }
+
 Array.from(answerButtonsEl.children).forEach(button => {
     setStatusClass(button, button.dataset.correct)
 })
 
-if (shuffledQuestions.length > currentQuestionIndex + 1) {
+if (shuffledQuestions > currentQuestionIndex + 1) {
     nextButton.classList.remove("hide")
     checkAnswerEl.classList.remove,("hide")
 } else {
@@ -168,20 +207,78 @@ function clearStatusClass(element) {
     element.classList.add("wrong");
 }
 
-
-// Added reset button
-var restartButton = document.querySelector(".restartButton");
-
-function restartGame() {
-  // Resets win and loss counts
-  correctCounter = 0;
-  wrongCounter = 0;
-  // Renders win and loss counts and sets them into client storage
-  setCorrects()
-  setWrongs()
+// this saves score //
+function saveScore() {
+    clearInterval(timerID);
+    timerEl.textContent = "Time: " + timeLeft;
+    setTimeout(function () {
+        questionContainerEl.classList.add("hide");
+        document.getElementById("score-container").classList.remove("hide");
+        document.getElementById("your-score").textContent = "Your Final Score is " + timeLeft;
+    }, 1000)
 }
 
+// get score from local storage //
+var loadScores = function () {
+    if (!savedScores) {
+        return false;
+    }
+    saveScore = JSON.parse(saveScore);
+    var initials = document.querySelector("#initials-field").ariaValueMax;
+    var newScore = {
+        score: timeLeft,
+        initials: initials
+    }
+    saveScore.push(newScore);
+    console.log(saveScore)
+
+    saveScore.forEach(score => {
+        initialsField.innerText = score.initials
+        scoreField.innerText = score.score
+    })
+}
+
+function showHighScores(initials) {
+    document.getElementById("highscores").classList.remove("hide")
+    document.getElementById("score-container").classList.add("hide");
+
+    startContainerEl.classList.add("hide");
+    questionContainerEl.classList.add("hide");
+    if (typeof initials == "string") {
+        var score = {
+            initials, timeLeft
+        }
+        scores.push(score)
+    }
+    var highScoreEl = document.getElementById("highscore");
+    highScoreEl.innerHTML = "";
+    for (i = 0; i < scores.length; i++) {
+        var section1 = document.createElement("section");
+        section1.setAttribute("class", "name-section");
+        section1.innerText = scores[i].initials;
+        var section2 = document.createElement("section");
+        section2.setAttribute("class", "score-section");
+        section2.innerText = scores[i].timeLeft;
+
+        highScoreEl.appendChild(section1);
+        highScoreEl.appendChild(section2);
+    }
+    localStorage.setItem("scores", JSON.stringify(scores));
+}
+
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault()
+    var initials = document.querySelector("#initials-field").value;
+    showHighScores(initials);
+})
 
 
+// Added reset button
+restartButton.addEventListener("click", function() {
+    window.location.reload();
+});
 
-restartButton.addEventListener("click", restartGame);
+clearScoreButton.addEventListener("click", function() {
+    localStorage.clear();
+    document.querySelector("#highscore").innerHTML = "";
+})
